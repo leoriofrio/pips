a9ee865da7c1ccbb2ba5d833f1bde0310553b7b0
jogaec22 <jogaec22@gmail.com>
2020-06-17 20:20:10 -0500

    transformacion de nombres de cabeceras para json data

--- src/app/configuration/product/product.component.ts
+++ src/app/configuration/product/product.component.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ProductColumns, TypeRegion } from '../../app.keys';
+import { ProductColumns, TypeRegion, TransformColumns } from '../../app.keys';
 import { ProjectComponent } from '../../shared/components/project/project.component';
 import { COLUMNS_PRODUCT, COLUMNS_PINNED_TOP_DATA } from './productColumns';
 import { ExcelExportService } from '../../shared/service/export-excel.service';
@@ -15,7 +15,7 @@ import { ProductService } from 'src/app/shared/service/product.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, TransformColumns {
   @ViewChild(ProjectComponent, {static: true}) child: ProjectComponent;
   
   
@@ -79,12 +79,31 @@ export class ProductComponent implements OnInit {
   }
 
   public onJsonData(jsonData){    
-    console.log('data de Producto es', JSON.stringify(jsonData));
-    return this.productService.createProduct('1', JSON.stringify(jsonData));
+    console.log('data de Producto es', JSON.stringify(jsonData));    
+    let jsonFinal = this.namesToProps(jsonData);  
+    console.log('jsonFinal', jsonFinal);  
+    return this.productService.createProduct('1', JSON.stringify(jsonFinal));
   }
 
   public getDataProduct() {
     return this.productService.getProductByRegion(TypeRegion.SIERRA);
+  }
+
+  public namesToProps(json){
+    return json.map(key => {      
+      let res = {};
+      res[`${ProductColumns.ID.prop}`] = key[ProductColumns.ID.name];
+      res[`${ProductColumns.COD.prop}`] = key[ProductColumns.COD.name];
+      res[`${ProductColumns.SUBLINE.prop}`] = key[ProductColumns.SUBLINE.name];
+      res[`${ProductColumns.DESCRIPTION.prop}`] = key[ProductColumns.DESCRIPTION.name];
+      res[`${ProductColumns.SERIE.prop}`] = key[ProductColumns.SERIE.name];
+      res[`${ProductColumns.NIVEL.prop}`] = key[ProductColumns.NIVEL.name];
+      res[`${ProductColumns.DEGREE.prop}`] = key[ProductColumns.DEGREE.name];
+      res[`${ProductColumns.BUSINESS_LINE.prop}`] = key[ProductColumns.BUSINESS_LINE.name];
+      res[`${ProductColumns.ISBN.prop}`] = key[ProductColumns.ISBN.name];
+      res[`${ProductColumns.REGION.prop}`] = key[ProductColumns.REGION.name];
+      return res;
+    });
   }
 
 }
